46558db3f77bec7abfa55433a957407bf24d04c4
jje <developerjieun94@gmail.com>
2024-05-29 14:52:10 +0900

    project setting && initial commit !

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container py-5 px-11">
+      <Navbar />
     </div>
   );
+}
+
+function Navbar() {
+  return <div className="text-4xl">Text Summarizer 📝</div>;
 }
 
 export default App;
